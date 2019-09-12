1f93400bf0c23b85b34e1120841aa8e77e3884df
Alexandra Pini <alexandrampini@gmail.com>
2019-09-11 23:20:51 -0400

    link to db.js

--- server.js
+++ server.js
@@ -6,4 +6,16 @@ app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/api/products', async (req, res, next) => {
+  try {
+    res.send(await db.findAll());
+  }
+  catch(ex) {
+    next(ex);
+  }
+});
+
+
+
+
 app.listen(4000, () => console.log('listening on port 4000'));
