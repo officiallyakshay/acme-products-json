e398ee1e97e225cf622cf3e03355253e5b007b0e
Alexandra Pini <alexandrampini@gmail.com>
2019-09-11 23:34:44 -0400

    const db

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const path = require ('path');
 const app = express();
 
+const db = require ('./db')('./products.json', (item, items) => {
+  if (!item.name) {
+    return 'name is required';
+  }
+  if (items.map( i => i.name ).includes(item.name)) {
+    return 'name must be unique';
+  }
+});
+
+app.use(express.json());
+
 app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -12,6 +23,15 @@ app.get('/api/products', async (req, res, next) => {
   }
   catch(ex) {
     next(ex);
+  }
+});
+
+app.post('/api/products', async (req, res, next) => {
+  try {
+    res.send(await db.create(req.body));
+  }
+  catch (ex) {
+    next (ex);
   }
 });
 
